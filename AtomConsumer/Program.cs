072f5e475673f92323d2c9753762d5b92c36abd4
Johnny Graber <jg@jgraber.ch>
2015-09-13 12:47:30 +0200

    use the in .Net built-in Atom feed reader

--- AtomConsumer/Program.cs
+++ AtomConsumer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace AtomConsumer
 {
@@ -10,6 +12,18 @@ namespace AtomConsumer
     {
         static void Main(string[] args)
         {
+            Atom10FeedFormatter formatter = new Atom10FeedFormatter();
+            using (XmlReader reader = XmlReader.Create("http://improveandrepeat.com/feed/atom/"))
+            {
+                formatter.ReadFrom(reader);
+            }
+
+            foreach (SyndicationItem item in formatter.Feed.Items)
+            {
+                Console.WriteLine("[{0}][{1}] {2}", item.PublishDate, item.Title.Text, item.Links.First().Uri);
+            }
+
+            Console.ReadLine();
         }
     }
 }
