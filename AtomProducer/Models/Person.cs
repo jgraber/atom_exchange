282e28017fd616df51ec05052bc20b1bab49c90c
Johnny Graber <jg@jgraber.ch>
2015-09-28 21:30:55 +0200

    add Person class

---
+++ AtomProducer/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace AtomProducer.Models
+{
+    public class Person
+    {   
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; internal set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
